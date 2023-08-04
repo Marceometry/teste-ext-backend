17e695ae328eed3091139fba5aa9d880fa646229
Marcelino Teixeira <marcelino.teixeira.dev@gmail.com>
2023-08-04 16:49:14 -0300

    feat: add users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmConfigModule } from './typeorm/typeorm.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmConfigModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    return this.usersRepository.save({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+  }
+
+  findAll() {
+    return this.usersRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.usersRepository.findOneBy({ id });
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return this.usersRepository.update(id, updateUserDto);
+  }
+
+  remove(id: number) {
+    return this.usersRepository.delete(id);
+  }
+}
